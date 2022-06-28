ae893f1cae086f12d240004302cbd15ef93c187c
nvcho42 <nachonico42@gmail.com>
2022-06-28 10:40:03 -0400

    xd

---
+++ bd_registro.sql
@@ -0,0 +1,9 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+04:00";
+
+CREATE TABLE REGISTRO(
+  id_huella varchar(30) NOT NULL ,
+  temperatura float(4) ,
+  fecha_hora datetime
+
+);
